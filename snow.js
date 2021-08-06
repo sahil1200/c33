0278e8f7ca8ade848662fb7295feb7793592e6df
sahil1200 <84191427+sahil1200@users.noreply.github.com>
2021-08-06 19:36:18 +0530

    Add files via upload

---
+++ snow.js
@@ -0,0 +1,19 @@
+class Snow {
+    constructor(x,y,r){
+        var option = {restitution:0.4}
+        this.r = r 
+        this.image = loadImage ("snow4.webp")
+        this.body = Bodies.circle(x,y,this.r,option)
+        World.add(world,this.body)
+    }
+    display (){
+        var pos = this.body.position
+        var angle= this.body.angle
+        push()
+        translate(pos.x,pos.y)
+        rotate(angle)
+        imageMode(CENTER)
+        image(this.image,0,0,this.r*2,this.r*2)
+        pop()
+    }
+}
